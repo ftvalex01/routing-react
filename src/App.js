e22237b4eb8f161a8163c9491b12520a90feefb1
alejandro <ftvalex@hotmail.com>
2021-11-04 12:40:28 +0000

    Rutas con Parametros + hooks
    Practicando React , Usando rutas con parametros.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {
 import Contacto from './components/Contacto';
 import Home from './components/Home';
 import Nosotros from './components/Nosotros';
-
+import User from './components/User';
 
 
 
@@ -22,11 +22,12 @@ function App() {
       <h1>React-Router </h1>
   <BrowserRouter>
   <Link to="/" className="btn btn-dark" >Inicio</Link>
-  <NavLink to="/nosotros" className="btn btn-dark mx-3" activeClassName="active-link">Nosotros</NavLink>
-  <NavLink to="/contacto" className="btn btn-dark" activeClassName="selected">Contacto</NavLink>
+  <NavLink to="/nosotros" className="btn btn-dark mx-3" >Nosotros</NavLink>
+  <NavLink to="/contacto" className="btn btn-dark" >Contacto</NavLink>
     <Routes>
       <Route path="/" element={<Home/>}></Route>
       <Route path="/nosotros" element={<Nosotros/>}></Route>
+      <Route path="/nosotros/:id" element={<User/>}></Route>
       <Route path="/contacto" element={<Contacto/>}></Route>
     </Routes>
   </BrowserRouter>,
--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Nosotros = () => {
 
@@ -12,9 +13,9 @@ const Nosotros = () => {
     }, [])
 
     const llamadaApi = async()=>{
-       const data = await fetch('https://jsonplaceholder.typicode.com/users')
+       const data = await fetch('https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations')
        const users = await data.json()
-       setEquipo(users)
+       setEquipo(users.civilizations)
     }
     
 
@@ -24,7 +25,8 @@ const Nosotros = () => {
             <ul>
             {
                 equipo.map(item =>(
-                    <li key={item.id}>{item.name}-{item.email}</li>
+                    <li key={item.id}>
+                    <Link to={`/nosotros/${item.id}`}>{item.name}-{item.expansion}</Link></li>
                 )
                  )
             }
